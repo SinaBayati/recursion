0e0cb68443afc1943129f33bf9f8bc869f1f240b
Sina Bayati <bayati.sina.w@gmail.com>
2024-06-01 23:50:30 +0330

    Implement fibonacci function recursively

--- fibonacci.js
+++ fibonacci.js
@@ -25,3 +25,21 @@ function fibs(n){
   }
   return result;
 }
+
+function fibsRec(n){
+  if (n < 1) throw new Error("n must be equal to or greater than 1");
+  
+  const result = [];
+
+  for (let i = 1; i <= n; i++) {
+    result.push(fib(i));
+  }
+
+  return result;
+}
+
+function fib(n){
+  if(n === 1) return 0;
+  if(n === 2) return 1;
+  return fib(n - 1) + fib(n - 2);
+}
